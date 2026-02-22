c64826287f82d6ee4f23f79c261891109b9be0af
Nithin Yanna <nithinyanna10@users.noreply.github.com>
2026-02-22 14:26:14 -0500

    env: register MicroserviceRoutingHard-v0

--- env/__init__.py
+++ env/__init__.py
@@ -3,6 +3,7 @@
 import gymnasium as gym
 
 from env.microservice_env import MicroserviceRoutingEnv
+from env.microservice_env_hard import MicroserviceRoutingHardEnv
 
 gym.register(
     id="MicroserviceRouting-v0",
@@ -10,4 +11,10 @@ gym.register(
     max_episode_steps=1000,
 )
 
-__all__ = ["MicroserviceRoutingEnv"]
+gym.register(
+    id="MicroserviceRoutingHard-v0",
+    entry_point="env.microservice_env_hard:MicroserviceRoutingHardEnv",
+    max_episode_steps=2000,
+)
+
+__all__ = ["MicroserviceRoutingEnv", "MicroserviceRoutingHardEnv"]
